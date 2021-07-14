69f5b244b3ae7f6e2e2fcf82e65b8f47a2e4324e
JBragaV <44869203+JBragaV@users.noreply.github.com>
2021-07-14 11:07:53 -0300

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+import { Nudes } from '../src/components/Nudes'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function FotoSideBar(props) {
+  return (
+    <Box>
+      <img style = {{borderRadius: '8px'}} src={`https://github.com/${props.user}.png`}alt='Apontadores'/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const user = 'JBragaV'
+  const fotosApontado = ['Jo', 'Dadada', 'Manuela']
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="apontador" style = {{gridArea: 'apontadores' }}>
+          <FotoSideBar user = {user}/>
+        </div>
+        <div className="dadada" style = {{gridArea: 'dadada' }}>
+          <Box>DADADA</Box>
+        </div>
+        <div className="nudes" style = {{gridArea: 'nudes' }}>
+          <Nudes>
+            Nudes ({fotosApontado.length})
+            {fotosApontado.map(item => <li>{item}</li>)}
+          </Nudes>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
